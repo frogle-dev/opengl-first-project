ae9bd79537f06e9008606157655fe4015dbdf5be
frogle-dev <jonah0602@outlook.com>
2025-11-17 12:10:50 -0800

    simple cam movement

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,10 @@ void processInput(GLFWwindow* window);
 void generateTexture(const char* path, unsigned int &id, bool RGBA);
 
 
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
 int main()
 {
     // initialization
@@ -42,10 +46,8 @@ int main()
     }
 
     // rendering stuff
-    unsigned int texture1, texture2;
-    generateTexture("../images/container.jpg", texture1, false);
-    stbi_set_flip_vertically_on_load(true);
-    generateTexture("../images/awesomeface.png", texture2, true);
+    unsigned int texture1;
+    generateTexture("../images/dirt.png", texture1, false);
 
     Shader shader("/home/jonah/Programming/Opengl/opengl-first-project/src/vertex.glsl", "/home/jonah/Programming/Opengl/opengl-first-project/src/fragment.glsl");
 
@@ -104,18 +106,6 @@ int main()
         -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f
     };
     
-    glm::vec3 cubePositions[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f), 
-        glm::vec3( 2.0f,  5.0f, -15.0f), 
-        glm::vec3(-1.5f, -2.2f, -2.5f),  
-        glm::vec3(-3.8f, -2.0f, -12.3f),  
-        glm::vec3( 2.4f, -0.4f, -3.5f),  
-        glm::vec3(-1.7f,  3.0f, -7.5f),  
-        glm::vec3( 1.3f, -2.0f, -2.5f),  
-        glm::vec3( 1.5f,  2.0f, -2.5f), 
-        glm::vec3( 1.5f,  0.2f, -1.5f), 
-        glm::vec3(-1.3f,  1.0f, -1.5f)  
-    };
 
     unsigned int VAO; //vertex array object
     glGenVertexArrays(1, &VAO);
@@ -150,7 +140,6 @@ int main()
     // setting texture samplers in frag shader to corresponding texture units
     shader.use();
     shader.setInt("texture1", 0);
-    shader.setInt("texture2", 1);
 
 
     // render loop
@@ -165,13 +154,16 @@ int main()
 
         glActiveTexture(GL_TEXTURE0); 
         glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
 
         glBindVertexArray(VAO);
 
+
         glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        view = glm::lookAt(
+            cameraPos,
+            cameraPos + cameraFront,
+            cameraUp
+        );
 
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), 1280.0f / 720.0f, 0.1f, 100.0f);
@@ -179,15 +171,17 @@ int main()
         shader.setMat4("view", view);
         shader.setMat4("projection", projection);
 
-        for (unsigned int i = 0; i < 10; i++)
+        for (unsigned int x = 0; x < 10; x++)
         {
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 0.5f, 0.2f));
+            for (unsigned int y = 0; y < 10; y++)
+            {
+                glm::mat4 model = glm::mat4(1.0f);
+                model = glm::translate(model, glm::vec3(1.0f * x, 1.0f * y, 0.0f));
 
-            shader.setMat4("model", model);
+                shader.setMat4("model", model);
 
-            glDrawArrays(GL_TRIANGLES, 0, 36);
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+            }
         }
     //    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -217,7 +211,7 @@ void processInput(GLFWwindow* window)
     }
 
     bool wireframe = false;
-    if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+    if(glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
     {
         wireframe = !wireframe;
     }
@@ -230,6 +224,25 @@ void processInput(GLFWwindow* window)
     {
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     }
+
+    // cam movement
+    const float cameraSpeed = 0.05f;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        cameraPos += cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        cameraPos -= cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        cameraPos -= cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        cameraPos += cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+    }
 }
 
 void generateTexture(const char* path, unsigned int &id, bool RGBA)
@@ -239,8 +252,8 @@ void generateTexture(const char* path, unsigned int &id, bool RGBA)
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     int width, height, nrChannels;
     unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
