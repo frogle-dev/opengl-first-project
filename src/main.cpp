3cd950ba54fa2d94d4f4404bb9b2c5de0b79df31
frogle-dev <jonah0602@outlook.com>
2025-11-16 19:41:45 -0800

    mulitple cubes

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
-    GLFWwindow* window = glfwCreateWindow(600, 600, "First OpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "First OpenGL", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -103,6 +103,19 @@ int main()
         -0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 0.0f,
         -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f
     };
+    
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 2.0f,  5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),  
+        glm::vec3( 2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f,  3.0f, -7.5f),  
+        glm::vec3( 1.3f, -2.0f, -2.5f),  
+        glm::vec3( 1.5f,  2.0f, -2.5f), 
+        glm::vec3( 1.5f,  0.2f, -1.5f), 
+        glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
 
     unsigned int VAO; //vertex array object
     glGenVertexArrays(1, &VAO);
@@ -139,9 +152,6 @@ int main()
     shader.setInt("texture1", 0);
     shader.setInt("texture2", 1);
 
-    unsigned int modelLoc = glGetUniformLocation(shader.shaderProgramID, "model");
-    unsigned int viewLoc = glGetUniformLocation(shader.shaderProgramID, "view");
-    unsigned int projectionLoc = glGetUniformLocation(shader.shaderProgramID, "projection");
 
     // render loop
     while(!glfwWindowShouldClose(window))
@@ -160,20 +170,25 @@ int main()
 
         glBindVertexArray(VAO);
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 0.5f, 0.0f));
-
         glm::mat4 view = glm::mat4(1.0f);
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
         glm::mat4 projection;
-        projection = glm::perspective(glm::radians(45.0f), 600.0f / 600.0f, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(45.0f), 1280.0f / 720.0f, 0.1f, 100.0f);
 
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        shader.setMat4("view", view);
+        shader.setMat4("projection", projection);
 
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 0.5f, 0.2f));
+
+            shader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
     //    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
--- src/shader.hpp
+++ src/shader.hpp
@@ -97,6 +97,10 @@ public:
     {
         glUniform1f(glGetUniformLocation(shaderProgramID, name.c_str()), value);
     }
+    void setMat4(const std::string &name, glm::mat4 value) const
+    {
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgramID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+    }
 
 private:
     void checkCompileErrors(unsigned int shader, std::string type)
